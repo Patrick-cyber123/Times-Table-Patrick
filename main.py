6ae617ffc1d6e77dbe6c8af693ab21bc003ef5b4
Patrick-cyber123 <patrick.tumbaga@stu.ocsb.ca>
2024-12-10 15:44:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# ask number
+num =  int(input("Enter a number: "))
+
+# initialize
+count = 10
+
+# mutiply through the numbers from 10 to 20
+while (count<= 20):
+  print(str(count) + " x " + str(num) + " = " + str(count * num))
+  count = count + 1
